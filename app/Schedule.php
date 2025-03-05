a54b28d5b2989e35e3a00c7d07f69f1f315b6dc0
iphone-taro <saga.siga.noga@gmail.com>
2025-03-05 16:42:07 +0900

    no message

--- app/Schedule.php
+++ app/Schedule.php
@@ -52,8 +52,8 @@ class Schedule {
         $res = $newData->save();
 
         //起動時が5時かどうか
-        $chStart = new DateTime('16:34:00');
-        $chEnd = new DateTime('16:36:00');
+        $chStart = new DateTime('16:44:00');
+        $chEnd = new DateTime('16:46:00');
 
         if ($chStart <= $now && $now < $chEnd) {
             echo "YES\n";
@@ -105,7 +105,7 @@ class Schedule {
             "\n" . 
             "本日のストグラでイチバン「好き嫌い.com」が盛り上がったのは！？\n" .
             "\n" .
-            "    【 " . $hour . "時" . $minute . "分 】\n" . 
+            "    【 " . $hour . "時" . $minute . "分 】（" . $maxCount . "投稿/2分間）\n" . 
             "\n" .
             "でしたー！\n" .
             "#ストグラ #ストグラフ\n" .
@@ -114,7 +114,6 @@ class Schedule {
             "https://sutograph.net";
 
             echo $tweetText;
-            $tweetText = "abc";
             //投稿処理
             $connection = new TwitterOAuth(
             Consts::API_KEY,
