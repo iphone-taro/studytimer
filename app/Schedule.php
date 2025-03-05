bf1d185c225423e1310662c498f4e5848da80b58
iphone-taro <saga.siga.noga@gmail.com>
2025-03-06 08:13:19 +0900

    no message

--- app/Schedule.php
+++ app/Schedule.php
@@ -52,10 +52,10 @@ class Schedule {
         $res = $newData->save();
 
         //起動時が5時かどうか
-        $chStart = new DateTime('08:12:00');
-        $chEnd = new DateTime('08:14:00');
-        // $chStart = new DateTime('05:00:00');
-        // $chEnd = new DateTime('05:02:00');
+        // $chStart = new DateTime('08:12:00');
+        // $chEnd = new DateTime('08:14:00');
+        $chStart = new DateTime('05:00:00');
+        $chEnd = new DateTime('05:02:00');
 
         if ($chStart <= $now && $now < $chEnd) {
             echo "YES\n";
